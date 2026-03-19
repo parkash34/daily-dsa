e90a4d974dac54fb78d7fd9dcbce67a2e3384318
parkash34 <ohm.parkash34@gmail.com>
2026-03-19 22:36:48 +0100

    Add Arrays DSA problems

---
+++ Arrays/SquaresOfASortedArray_977.java
@@ -0,0 +1,31 @@
+class SquaresOfASortedArray_977 {
+    public static int[] sortedSquares(int[] nums) {
+        int[] result = new int[nums.length];
+        int left = 0, right = nums.length - 1;
+        int pos = nums.length - 1; 
+
+        while (left <= right) {
+            int leftSq = nums[left] * nums[left];
+            int rightSq = nums[right] * nums[right];
+
+            if (leftSq > rightSq) {
+                result[pos] = leftSq;
+                left++;
+            } else {
+                result[pos] = rightSq;
+                right--;
+            }
+            pos--;
+        }
+
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] nums = {-4, -1, 0, 3, 10};
+        int[] result = sortedSquares(nums);
+        for (int num : result) {
+            System.out.print(num + " ");
+        }
+    }
+
+}
