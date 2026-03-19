d76a77694a588cf651ddeece77da79a47926578f
parkash34 <ohm.parkash34@gmail.com>
2026-03-19 22:40:58 +0100

    Add Two Sum II - Input Array Is Solve Problem

---
+++ Arrays/TwoSumIIInputArrayIsSorted_167.java
@@ -0,0 +1,26 @@
+public class TwoSumIIInputArrayIsSorted_167 {
+    public static int[] twoSum(int[] numbers, int target) {
+        int i = 0, j = numbers.length-1;
+        while(j > i){
+            if(numbers[i] + numbers[j] == target){
+                return new int[]{++i, ++j};
+            }
+            if(numbers[i] + numbers[j] > target){
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return null;
+    }
+    public static void print(int[] arr){
+        for(int i : arr){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int[] arr1 = {1,2,7,11,15};
+        print(twoSum(arr1, 9));
+    }
+}
